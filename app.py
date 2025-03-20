a481b3631d0d791dc936cfc61b3d6dfd24b59123
aalbaa28 <albafar28@gmail.com>
2025-03-20 21:30:48 +0100

    Actualizando JSONs

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import json
 import os
-from pandasai import PandasAI
+from pandasai.core import PandasAI
 from pandasai.llm.huggingface import HuggingFaceLLM
 
 # -------- CONFIGURAR IA
