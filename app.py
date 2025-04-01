158b7142f54b9155104451edd0f66bd02e20587d
aalbaa28 <albafar28@gmail.com>
2025-04-01 15:20:19 +0200

    Descripción de los cambios

--- app.py
+++ app.py
@@ -223,9 +223,6 @@ if json_data:
         if 'Date' in combined_df.columns:
             combined_df = combined_df[(combined_df['Date'] >= start_date) & (combined_df['Date'] <= end_date)]
 
-    # Mostrar DataFrame solo si se aplicaron los filtros
-    st.dataframe(combined_df)
-
 
     # Create tabs for each position
     tab1, tab2, tab3, tab4, tab5, tab6, tab7, tab8 = st.tabs(["Top", "Jgl", "Mid", "Adc", "Supp", "By Champion", "By Player", "AI Assistant"])
